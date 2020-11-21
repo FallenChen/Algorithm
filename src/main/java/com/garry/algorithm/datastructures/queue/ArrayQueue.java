07909adf44cb81d6745d1fdd0de470c9c049ec27
GarryChen <chenymj23@gmail.com>
2020-11-21 21:58:26 +0800

    add comment

--- src/main/java/com/garry/algorithm/datastructures/queue/ArrayQueue.java
+++ src/main/java/com/garry/algorithm/datastructures/queue/ArrayQueue.java
@@ -1,7 +1,11 @@
 package com.garry.algorithm.datastructures.queue;
 
 /**
+ * 除了泛型，size属性的缺失是ArrayQueue和IntQueue的另一个区别。ArrayQueue的size，是由公式，根据空状态和满状态计算出
  * 
+ * ArrayQueue最大的size是data.length-1。如果吧数组看成圆形，那么从逻辑上讲，变量 rear的位置总是在变量 front的前面。
+ * 所以，rear和front 之间组成的状态数，就是data array的长度
+ * 而，所有的状态中的一个，用来判断queue是空还是满
  */
 public class ArrayQueue<T> implements Queue<T> {
 
@@ -58,12 +62,17 @@ public class ArrayQueue<T> implements Queue<T> {
 
     @Override
     public boolean isEmpty() {
-        
+        // 想象成圆形
         return rear == front;
     }
-    
+    // 满的时候，就是rear刚好在front前面 所以下面其实是 1 % data.length
     public boolean isFull(){
         return (front + data.length - rear) % data.length == 1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(1 % 9);
     }
 
 }
