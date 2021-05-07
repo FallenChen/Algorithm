136fe38caa21a2918f1369be27321af23a0828bd
GarryChen <chenymj23@gmail.com>
2021-05-07 17:08:28 +0800

    restart

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # algorithm
-not only for interview, practise abstraction ability
 
-刷题没有意义，程序员是工匠，需要打磨手艺，数据结构和算法是最好的材料
+Just for fun
---
+++ src/main/java/leetcode/regular_expression_matching/java/Solution.java
@@ -0,0 +1,58 @@
+package leetcode.regular_expression_matching.java;
+
+/**
+ * @ClassName Solution
+ * @Description
+ * @Author cy
+ * @Date 2021/5/7 16:30
+ */
+public class Solution {
+
+    /**
+     * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*' where:
+     *
+     *     '.' Matches any single character.
+     *     '*' Matches zero or more of the preceding element.
+     *
+     * The matching should cover the entire input string (not partial).
+     * @link https://leetcode.com/problems/regular-expression-matching/
+     * @param text
+     * @param pattern
+     * @return
+     */
+    public static boolean isMatch(String text, String pattern) {
+
+        if(pattern.isEmpty())
+        {
+            return text.isEmpty();
+        }
+        // case for '.'
+        boolean firstMatch = (!text.isEmpty() &&
+                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+        // case for '*','*' can't be the first
+        if(pattern.length() >= 2 && pattern.charAt(1) == '*')
+        {
+            // aa , a*
+            boolean firstCase = firstMatch && isMatch(text.substring(1), pattern);
+            // ab, .* match any string
+            boolean secondCase = isMatch(text,pattern.substring(2));
+            return firstCase || secondCase;
+
+        }
+        else
+        {
+            return firstMatch && isMatch(text.substring(1),pattern.substring(1));
+        }
+
+    }
+
+    public static void main(String[] args) {
+//        boolean match = isMatch("aa", "a");
+//        boolean match = isMatch("aa", "a*");
+//        boolean match = isMatch("ab", ".*");
+//        boolean match = isMatch("aab", "c*a*b");
+        boolean match = isMatch("mississippi", "mis*is*p*.");
+        System.out.println(match);
+    }
+}
