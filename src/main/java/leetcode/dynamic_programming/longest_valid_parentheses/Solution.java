ae07f323e3cb393c95cf8314145458de22aaa804
GarryChen <chenymj23@gmail.com>
2021-05-26 20:30:55 +0800

    longest_valid_parentheses-Brute dp

--- src/main/java/leetcode/dynamic_programming/longest_valid_parentheses/Solution.java
+++ src/main/java/leetcode/dynamic_programming/longest_valid_parentheses/Solution.java
@@ -30,15 +30,40 @@ public class Solution {
      */
     public int longestValidParentheses(String s) {
         // Brute Force
+//        int maxlen = 0;
+//        for(int i=0; i<s.length();i++)
+//        {
+//            for(int j=i+2; j<=s.length();j+=2)
+//            {
+//                if(isValid(s.substring(i,j)))
+//                {
+//                    maxlen = Math.max(maxlen,j-i);
+//                }
+//            }
+//        }
+//        return maxlen;
+
+        // Using Dynamic Programming
         int maxlen = 0;
-        for(int i=0; i<s.length();i++)
+        int dp[] = new int[s.length()];
+        for(int i=1; i<s.length();i++)
         {
-            for(int j=i+2; j<=s.length();j++)
+            if(s.charAt(i) == ')')
             {
-                if(isValid(s.substring(i,j)))
+
+                if(s.charAt(i-1) == '(')
                 {
-                    maxlen = Math.max(maxlen,j-i);
+                    int count = i>=2 ? dp[i-2] : 0;
+                    dp[i] = count + 2;
                 }
+
+
+                else if((i-dp[i-1] > 0) && (s.charAt(i-dp[i-1]-1) == '('))
+                {
+                    int count = (i-dp[i-1]) >= 2? dp[i - dp[i - 1] - 2] : 0;
+                    dp[i] = dp[i-1] + count + 2;
+                }
+                maxlen = Math.max(maxlen,dp[i]);
             }
         }
         return maxlen;
@@ -62,5 +87,13 @@ public class Solution {
             }
         }
         return stack.empty();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        int i = solution.longestValidParentheses("(()");
+//        int i = solution.longestValidParentheses("())((()())");
+        int i = solution.longestValidParentheses("(()())");
+        System.out.println(i);
     }
 }
