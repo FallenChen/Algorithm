f1a4b21d251dfbd10f22c20414afd8f6d4b32b34
GarryChen <chenymj23@gmail.com>
2021-06-01 22:13:12 +0800

    candy init

---
+++ src/main/java/leetcode/greedy/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.greedy;
+
+/**
+ * @author cy
+ * @className Solution
+ * @description TODO
+ * @date 2021/6/1 21:30
+ */
+public class Solution {
+
+    /**
+     * There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+     *
+     * You are giving candies to these children subjected to the following requirements:
+     *
+     *     Each child must have at least one candy.
+     *     Children with a higher rating get more candies than their neighbors.
+     *
+     * Return the minimum number of candies you need to have to distribute the candies to the children.
+     *
+     * Input: ratings = [1,0,2]
+     * Output: 5
+     * Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+     *
+     * Input: ratings = [1,2,2]
+     * Output: 4
+     * Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+     * The third child gets 1 candy because it satisfies the above two conditions.
+     *
+     * @param ratings
+     * @return
+     */
+    public int candy(int[] ratings)
+    {
+
+    }
+}
