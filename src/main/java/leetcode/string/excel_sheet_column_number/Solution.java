efc2164499cf65792be1a0ac7aa1295b21ad398c
GarryChen <chenymj23@gmail.com>
2021-06-28 17:15:17 +0800

    excel_sheet_column_number

---
+++ src/main/java/leetcode/string/excel_sheet_column_number/Solution.java
@@ -0,0 +1,45 @@
+package leetcode.string.excel_sheet_column_number;
+
+/**
+ * @author cy
+ * @className Solution
+ * @description TODO
+ * @date 2021/6/28 16:48
+ */
+public class Solution {
+
+    /**
+     * Given a string columnTitle that represents the column title as appear in an Excel sheet,
+     * return its corresponding column number.
+     *
+     * A -> 1
+     * B -> 2
+     * C -> 3
+     * ...
+     * Z -> 26
+     * AA -> 27
+     * AB -> 28
+     * ...
+     *
+     * Input: columnTitle = "A"
+     * Output: 1
+     *
+     * Input: columnTitle = "AB"
+     * Output: 28
+     *
+     * Input: columnTitle = "ZY"
+     * Output: 701
+     *
+     * Input: columnTitle = "FXSHRXW"
+     * Output: 2147483647
+     *
+     * @param columnTitle
+     * @return
+     */
+    public int titleToNumber(String columnTitle) {
+
+        int reduce = columnTitle.chars().reduce(0, (r, ch) -> r * 26 + (ch - 'A' + 1));
+
+        return reduce;
+    }
+}
