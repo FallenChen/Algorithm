493683f0bd68538764d2a642990b1a920d8f7cbc
GarryChen <chenymj23@gmail.com>
2021-07-02 10:19:46 +0800

    linked_list_cycle

---
+++ src/main/java/leetcode/two_pointers/linked_list_cycle/ListNode.java
@@ -0,0 +1,20 @@
+package leetcode.two_pointers.linked_list_cycle;
+
+/**
+ * @author cy
+ * @className ListNode
+ * @description TODO
+ * @date 2021/7/2 10:12
+ */
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
