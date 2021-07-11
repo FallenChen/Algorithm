7b853a8d855a6c477c31f03dec4137232905aeb2
GarryChen <chenymj23@gmail.com>
2021-07-11 20:20:09 +0800

    super_ugly_number

--- src/main/java/leetcode/heap/super_ugly_number/Solution.java
+++ src/main/java/leetcode/heap/super_ugly_number/Solution.java
@@ -1,5 +1,7 @@
 package leetcode.heap.super_ugly_number;
 
+import java.util.Arrays;
+
 /**
  * @author cy
  * @className Solution
@@ -28,6 +30,52 @@ public class Solution {
      * @return
      */
     public int nthSuperUglyNumber(int n, int[] primes) {
+//        int[] ugly = new int[n];
+//        int[] idx = new int[primes.length];
+//
+//        ugly[0] = 1;
+//
+//        for(int i=1; i<n; i++)
+//        {
+//            // find next
+//            ugly[i] = Integer.MAX_VALUE;
+//            for(int j=0; j<primes.length;j++)
+//            {
+//                ugly[i] = Math.min(ugly[i], primes[j] * ugly[idx[j]]);
+//            }
+//            // slip duplicate
+//            for(int j=0; j<primes.length; j++)
+//            {
+//                while (primes[j] * ugly[idx[j]] <= ugly[i])
+//                {
+//                    idx[j]++;
+//                }
+//            }
+//        }
+//        return ugly[n-1];
 
+        int[] ugly = new int[n];
+        int[] idx = new int[primes.length];
+        int[] val = new int[primes.length];
+        Arrays.fill(val,1);
+
+        int next = 1;
+        for(int i=0; i<n; i++)
+        {
+            ugly[i] = next;
+
+            next = Integer.MAX_VALUE;
+            for(int j=0; j<primes.length; j++)
+            {
+                // skip duplicate and avoid extra multiplication
+                if(val[j] == ugly[i])
+                {
+                    val[j] = ugly[idx[j]++] * primes[j];
+                }
+                // find next ugly number
+                next = Math.min(next, val[j]);
+            }
+        }
+        return ugly[n-1];
     }
 }
