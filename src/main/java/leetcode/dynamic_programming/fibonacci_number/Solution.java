ed00936b0b52cdd93d83e65b6f136598c44e5913
GarryChen <chenymj23@gmail.com>
2021-10-30 19:49:52 +0800

    fibonacci_number

--- src/main/java/leetcode/dynamic_programming/fibonacci_number/Solution.java
+++ src/main/java/leetcode/dynamic_programming/fibonacci_number/Solution.java
@@ -85,4 +85,26 @@ public class Solution {
         return memoize(n);
     }
 
+    // dp
+//    public int fib(int n)
+//    {
+//        if(n < 2)
+//        {
+//            return n;
+//        }
+          // 1.确定dp数组
+//        int[] dp = new int[n+1];
+          // 3.dp数组初始化
+//        dp[0] = 0;
+//        dp[1] = 1;
+          //4.确定遍历顺序
+//        for(int i=2; i<=n; i++)
+//        {
+              // 2.递推公式
+//            dp[i] = dp[i-1] + dp[i-2];
+//        }
+//        return dp[n];
+          // 5.举例推到dp数组 0 1 1 2 3 5 8 13 21 34 55
+//    }
+
 }
