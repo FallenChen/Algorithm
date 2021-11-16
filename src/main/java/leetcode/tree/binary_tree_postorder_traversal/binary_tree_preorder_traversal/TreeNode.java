c62bdabfc49871dc2212995aef17252457426df8
GarryChen <chenymj23@gmail.com>
2021-11-16 19:43:18 +0800

    binary_tree_preorder_traversal done

---
+++ src/main/java/leetcode/tree/binary_tree_postorder_traversal/binary_tree_preorder_traversal/TreeNode.java
@@ -0,0 +1,25 @@
+package leetcode.tree.binary_tree_postorder_traversal.binary_tree_preorder_traversal;
+
+/**
+ * @author cy
+ * @className TreeNode
+ * @description
+ * @date 2021/11/15 19:52
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val)
+    {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right)
+    {
+        this.val = val;
+        this.right = right;
+        this.left = left;
+    }
+}
