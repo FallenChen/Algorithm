2ed0515862c51dbcc310cd7626b39a029ac10197
GarryChen <chenymj23@gmail.com>
2021-11-21 20:50:24 +0800

    prder traversal

---
+++ src/main/java/leetcode/tree/binary_tree_order_traversal/TreeNode.java
@@ -0,0 +1,25 @@
+package leetcode.tree.binary_tree_order_traversal;
+
+/**
+ * @author cy
+ * @className TreeNode
+ * @description
+ * @date 2021/11/15 19:52
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val)
+    {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right)
+    {
+        this.val = val;
+        this.right = right;
+        this.left = left;
+    }
+}
