e21e89600bf3ce3a09eec59860f0ca4e78d3c703
GarryChen <chenymj23@gmail.com>
2022-02-09 10:36:06 +0800

    validate_binary_search_tree

---
+++ src/main/java/leetcode/tree/validate_binary_search_tree/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.tree.validate_binary_search_tree;
+
+public class Solution {
+	
+	public boolean isValidBST(TreeNode root) {
+		return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE); 
+	}
+
+	private boolean isValidBST(TreeNode root, long minValue, long maxValue) {
+		if(root == null) return true;
+		if(root.val >= maxValue || root.val <= minValue) return false;
+		return isValidBST(root.left, minValue, root.val) && isValidBST(root.right,root.val, maxValue);
+	}
+}
