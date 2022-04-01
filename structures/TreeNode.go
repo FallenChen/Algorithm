1d9834b83eaf0dd89db5508341f4913fc3c95233
GarryChen <chenymj23@gmail.com>
2022-04-01 15:26:48 +0800

    dfs and tree

--- structures/TreeNode.go
+++ structures/TreeNode.go
@@ -46,3 +46,31 @@ func Ints2TreeNode(ints []int) *TreeNode {
 
 	return root
 }
+
+// Tree2ints 把 *TreeNode 按照行还原成 []int
+func Tree2ints(tn *TreeNode) []int {
+	res := make([]int, 0, 1024)
+
+	queue := []*TreeNode{tn}
+
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			nd := queue[i]
+			if nd == nil {
+				res = append(res, NULL)
+			} else {
+				res = append(res, nd.Val)
+				queue = append(queue, nd.Left, nd.Right)
+			}
+		}
+		queue = queue[size:]
+	}
+
+	i := len(res)
+	for i > 0 && res[i-1] == NULL {
+		i--
+	}
+
+	return res[:i]
+}
