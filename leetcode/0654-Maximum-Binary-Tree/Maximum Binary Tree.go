8cb850556d7000ecbfde0a702df7f96ffbae725b
GarryChen <chenymj23@gmail.com>
2022-04-27 20:17:20 +0800

    tree

---
+++ leetcode/0654-Maximum-Binary-Tree/Maximum Binary Tree.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+
+)
+
+type TreeNode = structures.TreeNode
+
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+    if len(nums) < 1 {
+	    return nil
+    }
+
+    index := findMax(nums)
+
+    root := &TreeNode{
+	    Val: nums[index],
+	    Left: constructMaximumBinaryTree(nums[:index]),
+	    Right: constructMaximumBinaryTree(nums[index+1:]),
+    }
+    return root;
+}
+
+func findMax(nums []int) int {
+	index := 0;
+	for i:=1; i<len(nums); i++ {
+		if nums[i] > nums[index] {
+			index = i
+		}
+	}
+	return index
+}
