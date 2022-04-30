f84b3c48ca39c94b786e3029f43674f01998f5b0
GarryChen <chenymj23@gmail.com>
2022-04-30 20:28:16 +0800

    tree and dp

---
+++ leetcode/0538-Convert-BST-to-Greater-Tree/Convert BST to Greater Tree.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "algorithm/structures"
+
+type TreeNode = structures.TreeNode
+
+func convertBST(root *TreeNode) *TreeNode {
+
+	if root == nil {
+		return root
+	}
+
+	sum := 0
+	dfs(root, &sum)
+	return root
+}
+
+func dfs(root *TreeNode, sum *int) {
+	if root == nil {
+		return
+	}
+
+	dfs(root.Right, sum)
+	root.Val += *sum
+	*sum = root.Val
+	dfs(root.Left,sum)
+}
