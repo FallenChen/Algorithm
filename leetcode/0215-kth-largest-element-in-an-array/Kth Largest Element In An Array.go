c2ea5aafb8af81cd3e19a12c51fdee89a90a30ce
GarryChen <chenymj23@gmail.com>
2022-05-08 21:52:03 +0800

    quick sort

--- leetcode/0215-kth-largest-element-in-an-array/Kth Largest Element In An Array.go
+++ leetcode/0215-kth-largest-element-in-an-array/Kth Largest Element In An Array.go
@@ -9,37 +9,75 @@ func findKthLargest(nums []int, k int) int {
 	// return nums[len(nums)-k]
 
 	// 2 
-	m := len(nums) - k + 1
-	return quickSelect(nums, 0, len(nums)-1, m)
+	// m := len(nums) - k + 1
+	// return quickSelect(nums, 0, len(nums)-1, m)
+
+	m := len(nums) - k
+	num := quick(nums, 0, len(nums)-1, m)
+	return num
 }
 
-func quickSelect(nums []int, left, right, i int) int {
-
+func quick (nums []int, left, right, k int) int {
 	q := partition(nums, left, right)
-	k := q - left + 1
-	if i == k {
-		return nums[q]
+	
+	
+	if q == k {
+	    return nums[q]
 	}
-
-	if i < k{
-		return quickSelect(nums, left, q-1, i)
+	
+	if q > k {
+	    return quick(nums, left, q-1, k)
 	} else {
-		return quickSelect(nums, q+1, right, i-k)
+	    return quick(nums, q+1, right, k)
 	}
-}
-
-func partition(nums []int, left,right int) int {
-
-	i := left - 1
-
-	for j := left; j < right; j++ {
-		if nums[j] <= nums[right] {
-			i++
-			nums[i], nums[j] = nums[j], nums[i]
+    }
+    
+    func partition(nums []int, left, right int) int {
+	
+	i, j, pivot := left,right, nums[left]
+	    
+	    for i < j {
+		for i < j && nums[j] >= pivot {
+		    j--
 		}
-	}
-	nums[i+1], nums[right] = nums[right], nums[i+1]
-	return i + 1
-}
+		for i < j && nums[i] <= pivot {
+		    i++
+		}
+		
+		nums[i], nums[j] = nums[j], nums[i]
+	    }
+	    
+	    nums[i], nums[left] = nums[left], nums[i]
+	return i
+    }
+
+// func quickSelect(nums []int, left, right, i int) int {
+
+// 	q := partition(nums, left, right)
+// 	k := q - left + 1
+// 	if i == k {
+// 		return nums[q]
+// 	}
+
+// 	if i < k{
+// 		return quickSelect(nums, left, q-1, i)
+// 	} else {
+// 		return quickSelect(nums, q+1, right, i-k)
+// 	}
+// }
+
+// func partition(nums []int, left,right int) int {
+
+// 	i := left - 1
+
+// 	for j := left; j < right; j++ {
+// 		if nums[j] <= nums[right] {
+// 			i++
+// 			nums[i], nums[j] = nums[j], nums[i]
+// 		}
+// 	}
+// 	nums[i+1], nums[right] = nums[right], nums[i+1]
+// 	return i + 1
+// }
 	
 
