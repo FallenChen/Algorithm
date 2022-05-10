9a67d493eba0e215754467cde7a573e8252dc039
GarryChen <chenymj23@gmail.com>
2022-05-10 20:31:36 +0800

    slide window

---
+++ leetcode/0000-others/longest-no-repeat-sub-array/Longest No Repeat Sub Array.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func maxLength( arr []int ) int {
+
+	maxLen , len, left := 0,0,-1
+
+	h := make(map[int]int)
+
+	for idx, right := range arr {
+		if valueIdx, ok := h[right]; !ok || h[right] == -1 {
+			len++
+		} else {
+			if valueIdx > left {
+				left = valueIdx
+			}
+		}
+		h[right] = idx
+
+		if maxLen < len {
+			maxLen = len
+		}
+	}
+
+	return maxLen
+}
