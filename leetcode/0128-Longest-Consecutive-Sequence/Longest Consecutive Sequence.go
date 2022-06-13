114376cda5a9ce1962b29e618c59f1677e360326
GarryChen <chenymj23@gmail.com>
2022-06-13 20:47:32 +0800

    refactor 128

--- leetcode/0128-Longest-Consecutive-Sequence/Longest Consecutive Sequence.go
+++ leetcode/0128-Longest-Consecutive-Sequence/Longest Consecutive Sequence.go
@@ -2,31 +2,51 @@ package leetcode
 
 func longestConsecutive(nums []int) int {
 
-	res, numMap := 0, map[int]int{}
+	// res, numMap := 0, map[int]int{}
 
-	for _,num :=range nums {
-		if numMap[num] == 0 {
-			left, right, len := 0,0,0
+	// for _,num :=range nums {
+	// 	if numMap[num] == 0 {
+	// 		left, right, len := 0,0,0
 
-			if numMap[num-1] > 0 {
-				left = numMap[num-1]
-			} else {
-				left = 0
+	// 		if numMap[num-1] > 0 {
+	// 			left = numMap[num-1]
+	// 		} else {
+	// 			left = 0
+	// 		}
+	// 		if numMap[num+1] > 0 {
+	// 			right = numMap[num+1]
+	// 		} else {
+	// 			right = 0
+	// 		}
+
+	// 		len = left + right + 1
+	// 		numMap[num] = len
+
+	// 		res = max(res, len)
+	// 		numMap[num-left] = len
+	// 		numMap[num+right] = len
+	// 	} else {
+	// 		continue
+	// 	}
+	// }
+	// return res
+
+	numSet := map[int]bool{}
+	for _, num := range nums {
+		numSet[num] = true
+	}
+	res := 0 
+	for num := range numSet {
+		if !numSet[num-1]{
+			currentNum := num
+			currentStreak := 1
+			for numSet[currentNum + 1]{
+				currentNum++
+				currentStreak++
 			}
-			if numMap[num+1] > 0 {
-				right = numMap[num+1]
-			} else {
-				right = 0
+			if res < currentStreak {
+				res = currentStreak
 			}
-
-			len = left + right + 1
-			numMap[num] = len
-
-			res = max(res, len)
-			numMap[num-left] = len
-			numMap[num+right] = len
-		} else {
-			continue
 		}
 	}
 	return res
