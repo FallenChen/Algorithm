8001d536d631777886ea9f57e56b7bbcba8cc03b
GarryChen <chenymj23@gmail.com>
2022-07-17 15:25:09 +0800

    tree

---
+++ leetcode/0106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/Construct Binary Tree from Inorder and Postorder Traversal.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import "algorithm/structures"
+
+type TreeNode = structures.TreeNode
+
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	postorderLen := len(postorder)
+	if len(inorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{Val: postorder[postorderLen-1]}
+	postorder = postorder[:postorderLen-1]
+	for pos, node := range inorder {
+		if node == root.Val {
+			root.Left = buildTree(inorder[:pos], postorder[:len(inorder[:pos])])
+			root.Right = buildTree(inorder[pos+1:], postorder[len(inorder[:pos]):])
+		}
+	}
+	return root
+}
