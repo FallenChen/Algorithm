7f85952554555b4c43a98d9279b30a03836c05db
GarryChen <chenymj23@gmail.com>
2022-07-19 08:25:56 +0800

    array

---
+++ leetcode/0000-others/poker-straight/isStraight.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import "sort"
+
+func isStraight(nums []int) bool {
+	sort.Ints(nums)
+	joker := 0
+	for i := 0; i < 4; i++ {
+		if nums[i] == 0 {
+			joker++
+		} else {
+			if nums[i] == nums[i+1] {
+				return false
+			}
+		}
+	}
+	return nums[4]-nums[joker] < 5
+}
