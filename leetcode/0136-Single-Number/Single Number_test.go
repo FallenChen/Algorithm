3b93d34a279a8d0b7e32dd15981a9280e46a76da
GarryChen <chenymj23@gmail.com>
2022-07-22 10:20:52 +0800

    136 test case

---
+++ leetcode/0136-Single-Number/Single Number_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "testing"
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "testcase", args: args{
+			nums: []int{4, 1, 2, 1, 2},
+		}, want: 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
