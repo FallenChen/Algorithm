f3b833695e8209091be1ff30524e91f79afff4db
GarryChen <chenymj23@gmail.com>
2022-07-26 20:58:55 +0800

    review and unit test

--- leetcode/0199-Binary-Tree-Right-Side-View/Binary Tree Right Side View_test.go
+++ leetcode/0199-Binary-Tree-Right-Side-View/Binary Tree Right Side View_test.go
@@ -2,60 +2,28 @@ package leetcode
 
 import (
 	"algorithm/structures"
-	"fmt"
+	"reflect"
 	"testing"
 )
 
-
-type question199 struct {
-	para199
-	ans199
-}
-
-// para 是参数
-// one 代表第一个参数
-type para199 struct {
-	one []int
-}
-
-// ans 是答案
-// one 代表第一个答案
-type ans199 struct {
-	one []int
-}
-
-func Test_Problem199(t *testing.T) {
-
-	qs := []question199{
-
-		// {
-		// 	para199{[]int{}},
-		// 	ans199{[]int{}},
-		// },
-
-		// {
-		// 	para199{[]int{1}},
-		// 	ans199{[]int{1}},
-		// },
-
-		{
-			para199{[]int{1, 2, 3, structures.NULL,5, structures.NULL, 4}},
-			ans199{[]int{1, 3, 4}},
-		},
-
-		// {
-		// 	para199{[]int{1, 2, 3, 4, structures.NULL, structures.NULL, 5}},
-		// 	ans199{[]int{1, 3, 5}},
-		// },
+func Test_rightSideView(t *testing.T) {
+	type args struct {
+		root *TreeNode
 	}
-
-	fmt.Printf("------------------------Leetcode Problem 199------------------------\n")
-
-	for _, q := range qs {
-		_, p := q.ans199, q.para199
-		fmt.Printf("【input】:%v      ", p)
-		root := structures.Ints2TreeNode(p.one)
-		fmt.Printf("【output】:%v      \n", rightSideView(root))
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{name: "test",
+			args: args{structures.Ints2TreeNode([]int{1, 2, 3, structures.NULL, 5, structures.NULL, 4})},
+			want: []int{1, 3, 4}},
 	}
-	fmt.Printf("\n\n\n")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rightSideView(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("rightSideView() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
