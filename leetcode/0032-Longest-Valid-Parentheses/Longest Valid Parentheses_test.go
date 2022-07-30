65f2cd54e0857b824fcef395fad48eedd27b2163
GarryChen <chenymj23@gmail.com>
2022-07-30 14:18:17 +0800

    stack

---
+++ leetcode/0032-Longest-Valid-Parentheses/Longest Valid Parentheses_test.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import "testing"
+
+func Test_longestValidParentheses(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "test",
+			args: args{s: ")()())"}, want: 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestValidParentheses(tt.args.s); got != tt.want {
+				t.Errorf("longestValidParentheses() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
