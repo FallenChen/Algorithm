1e6fbf18557795f1c84ab6ad7ce17f370c324b44
GarryChen <chenymj23@gmail.com>
2022-08-04 21:11:06 +0800

    linked list

---
+++ leetcode/0328-Odd-Even-Linked-List/Odd Even Linked List_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+	"reflect"
+	"testing"
+)
+
+func Test_oddEvenList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "test", args: args{structures.Ints2List([]int{1, 2, 3, 4, 5})},
+			want: structures.Ints2List([]int{1, 3, 5, 2, 4}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := oddEvenList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("oddEvenList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
