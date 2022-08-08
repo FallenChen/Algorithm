cc453e0e157af5748b3dcfb3f7e0f23a9b3a1cfd
GarryChen <chenymj23@gmail.com>
2022-08-08 18:50:58 +0800

    two pointers

--- leetcode/0042-Trapping-Rain-Water/Trapping Rain Water.go
+++ leetcode/0042-Trapping-Rain-Water/Trapping Rain Water.go
@@ -2,16 +2,21 @@ package leetcode
 
 func trap(height []int) int {
 
-	res, left, right, maxLeft, maxRight := 0,0,len(height)-1,0,0
-	for left <= right{
+	// 数组必有一个 ”山顶“（若有多个高度相同山顶，任取一个即可）。
+	//
+	//根据”木桶原理“，山顶左侧的元素的盛水量 ，由左侧最大值决定；山顶右侧元素的盛水量，由右侧最大值决定。
+	//
+	//双指针法的两个指针最终会停在 “山顶” 处。
+	res, left, right, maxLeft, maxRight := 0, 0, len(height)-1, 0, 0
+	for left <= right {
 		if height[left] <= height[right] {
 			if height[left] > maxLeft {
 				maxLeft = height[left]
-			}else {
+			} else {
 				res += maxLeft - height[left]
 			}
 			left++
-		}else {
+		} else {
 			if height[right] >= maxRight {
 				maxRight = height[right]
 			} else {
@@ -20,5 +25,5 @@ func trap(height []int) int {
 			right--
 		}
 	}
-	return res  
+	return res
 }
---
+++ leetcode/0042-Trapping-Rain-Water/Trapping Rain Water_test.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import "testing"
+
+func Test_trap(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test",
+			args: args{[]int{4, 2, 0, 3, 2, 5}},
+			want: 9},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trap(tt.args.height); got != tt.want {
+				t.Errorf("trap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
