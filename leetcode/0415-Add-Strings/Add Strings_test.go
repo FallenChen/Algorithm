446a2cc3c117cd385308c75f11c3b83a294f08bf
GarryChen <chenymj23@gmail.com>
2022-08-15 16:10:41 +0800

    string

---
+++ leetcode/0415-Add-Strings/Add Strings_test.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import "testing"
+
+func Test_addStrings(t *testing.T) {
+	type args struct {
+		num1 string
+		num2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test",
+			args: args{
+				num1: "123",
+				num2: "23",
+			},
+			want: "146",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addStrings(tt.args.num1, tt.args.num2); got != tt.want {
+				t.Errorf("addStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
