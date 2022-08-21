b2c18cf72612990754374cc3550ed1654df69780
GarryChen <chenymj23@gmail.com>
2022-08-21 10:30:18 +0800

    string

--- leetcode/0050-Powx-n/Pow_x_n.go
+++ leetcode/0050-Powx-n/Pow_x_n.go
@@ -1,6 +1,5 @@
 package leetcode
 
-
 func myPow(x float64, n int) float64 {
 
 	if n == 0 {
@@ -13,12 +12,12 @@ func myPow(x float64, n int) float64 {
 
 	if n < 0 {
 		n = -n
-		x = 1/x
+		x = 1 / x
 	}
 
 	tmp := myPow(x, n/2)
 
-	if n %2 == 0 {
+	if n%2 == 0 {
 		return tmp * tmp
 	}
 
