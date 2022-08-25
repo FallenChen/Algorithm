da592a604621f3fbe3bbeb69bfc3012a50f91990
GarryChen <chenymj23@gmail.com>
2022-08-25 14:57:03 +0800

    sqrt

---
+++ leetcode/0069-Sqrtx/Sqrt_x_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "testing"
+
+func Test_mySqrt(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test",
+			args: args{4},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mySqrt(tt.args.x); got != tt.want {
+				t.Errorf("mySqrt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
