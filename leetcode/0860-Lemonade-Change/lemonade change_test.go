616e963413cdd06af4ad65aa8aa8480621648f69
GarryChen <chenymj23@gmail.com>
2022-08-28 10:58:38 +0800

    greedy

---
+++ leetcode/0860-Lemonade-Change/lemonade change_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "testing"
+
+func Test_lemonadeChange(t *testing.T) {
+	type args struct {
+		bills []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test",
+			args: args{[]int{5, 5, 5, 10, 20}},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lemonadeChange(tt.args.bills); got != tt.want {
+				t.Errorf("lemonadeChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
