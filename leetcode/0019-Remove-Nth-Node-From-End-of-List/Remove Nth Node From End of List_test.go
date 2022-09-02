b20dcf98217c5457617eba0a191e17ef91eb6f88
GarryChen <chenymj23@gmail.com>
2022-09-02 21:02:40 +0800

    two pointer

---
+++ leetcode/0019-Remove-Nth-Node-From-End-of-List/Remove Nth Node From End of List_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+	"reflect"
+	"testing"
+)
+
+func Test_removeNthFromEnd(t *testing.T) {
+	type args struct {
+		head *ListNode
+		n    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "test",
+			args: args{head: structures.Ints2List([]int{1, 2, 3, 4, 5}), n: 2},
+			want: structures.Ints2List([]int{1, 2, 3, 5}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeNthFromEnd(tt.args.head, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeNthFromEnd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
