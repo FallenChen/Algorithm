6ca676479128815f7100660082ef965705796925
GarryChen <chenymj23@gmail.com>
2022-09-04 19:43:35 +0800

    list

---
+++ leetcode/0021-Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+	"reflect"
+	"testing"
+)
+
+func Test_mergeTwoLists(t *testing.T) {
+	type args struct {
+		list1 *ListNode
+		list2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "test",
+			args: args{list1: structures.Ints2List([]int{1, 2, 4}), list2: structures.Ints2List([]int{1, 3, 4})},
+			want: structures.Ints2List([]int{1, 1, 2, 3, 4, 4}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeTwoLists(tt.args.list1, tt.args.list2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mergeTwoLists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
