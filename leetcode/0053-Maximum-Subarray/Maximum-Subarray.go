60329b0dd487719dc5f1bceed2bf4b1c8a0d398b
GarryChen <chenymj23@gmail.com>
2022-09-12 14:23:33 +0800

    dp

--- leetcode/0053-Maximum-Subarray/Maximum-Subarray.go
+++ leetcode/0053-Maximum-Subarray/Maximum-Subarray.go
@@ -1,12 +1,23 @@
 package leetcode
 
 func maxSubArray(nums []int) int {
-    
+
+	// old
+	//dp := make([]int, len(nums))
+	//dp[0] = nums[0]
+	//
+	//maxSoFar := nums[0]
+	//for i := 1; i < len(nums); i++ {
+	//	dp[i] = max(dp[i-1]+nums[i], nums[i])
+	//	maxSoFar = max(maxSoFar, dp[i])
+	//}
+
+	// new
 	maxSoFar := nums[0]
 	maxEndingHere := nums[0]
 
 	for i := 1; i < len(nums); i++ {
-		maxEndingHere = max(nums[i],maxEndingHere+nums[i])
+		maxEndingHere = max(nums[i], maxEndingHere+nums[i])
 		maxSoFar = max(maxSoFar, maxEndingHere)
 	}
 
