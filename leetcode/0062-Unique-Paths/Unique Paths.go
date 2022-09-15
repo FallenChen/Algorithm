ea0de163b08e0f4bee79e451f32de9c398f93157
GarryChen <chenymj23@gmail.com>
2022-09-15 16:11:59 +0800

    dp

--- leetcode/0062-Unique-Paths/Unique Paths.go
+++ leetcode/0062-Unique-Paths/Unique Paths.go
@@ -1,22 +1,35 @@
 package leetcode
 
-
 func uniquePaths(m int, n int) int {
 
-	dp := make([][]int, n)
+	//dp := make([][]int, n)
+	//
+	//for i:=0; i<n; i++ {
+	//	dp[i] = make([]int, m)
+	//}
+	//
+	//for i:=0; i<n; i++ {
+	//	for j:=0; j<m; j++ {
+	//		if j==0 || i==0 {
+	//			dp[i][j] = 1
+	//			continue
+	//		}
+	//		dp[i][j] = dp[i-1][j] + dp[i][j-1]
+	//	}
+	//}
+	//return dp[n-1][m-1]
 
-	for i:=0; i<n; i++ {
-		dp[i] = make([]int, m)
+	// todo
+	dp := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		dp[i] = 1
 	}
 
-	for i:=0; i<n; i++ {
-		for j:=0; j<m; j++ {
-			if j==0 || i==0 {
-				dp[i][j] = 1
-				continue 
-			}
-			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[j] += dp[j-1]
 		}
 	}
-	return dp[n-1][m-1]
+	return dp[n-1]
 }
