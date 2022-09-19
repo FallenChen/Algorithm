ab7dd1c04b350b2efdc6554ffe59b61a1cf5dd79
GarryChen <chenymj23@gmail.com>
2022-09-19 09:43:06 +0800

    hard two pointer

--- leetcode/0076-Minimum-Window-Substring/Minimum Window Substring_test.go
+++ leetcode/0076-Minimum-Window-Substring/Minimum Window Substring_test.go
@@ -1,52 +1,31 @@
 package leetcode
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
-type question76 struct {
-	para76
-	ans76
-}
-
-// para 是参数
-// one 代表第一个参数
-type para76 struct {
-	s string
-	p string
-}
-
-// ans 是答案
-// one 代表第一个答案
-type ans76 struct {
-	one string
-}
-
-func Test_Problem76(t *testing.T) {
-
-	qs := []question76{
-
+func Test_minWindow(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
 		{
-			para76{"ADOBECODEBANC", "ABC"},
-			ans76{"BANC"},
-		},
-
-		{
-			para76{"a", "aa"},
-			ans76{""},
-		},
-
-		{
-			para76{"a", "a"},
-			ans76{"a"},
+			name: "test",
+			args: args{
+				s: "ADOBECODEBANC",
+				t: "ABC",
+			},
+			want: "BANC",
 		},
 	}
-	fmt.Printf("------------------------Leetcode Problem 76------------------------\n")
-
-	for _, q := range qs {
-		_, p := q.ans76, q.para76
-		fmt.Printf("【input】:%v       【output】:%v\n\n\n", p, minWindow(p.s, p.p))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minWindow(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("minWindow() = %v, want %v", got, tt.want)
+			}
+		})
 	}
-	fmt.Printf("\n\n\n")
 }
