704bc1a4083bb37167d08314a580ec19e23750ee
GarryChen <chenymj23@gmail.com>
2022-09-23 09:24:32 +0800

    list

---
+++ leetcode/0083-Remove-Duplicates-from-Sorted-List/Remove Duplicates from Sorted List_test.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+	"reflect"
+	"testing"
+)
+
+func Test_deleteDuplicates(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "test",
+			args: args{head: structures.Ints2List([]int{1, 1, 2})},
+			want: structures.Ints2List([]int{1, 2}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteDuplicates(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deleteDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
