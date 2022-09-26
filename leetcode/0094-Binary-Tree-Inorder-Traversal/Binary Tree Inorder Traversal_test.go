7167e2d9c8e6a28d8799c254daa8916b2d91ce53
GarryChen <chenymj23@gmail.com>
2022-09-26 09:40:39 +0800

    tree

---
+++ leetcode/0094-Binary-Tree-Inorder-Traversal/Binary Tree Inorder Traversal_test.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+	"reflect"
+	"testing"
+)
+
+func Test_inorderTraversal(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "test",
+			args: args{root: structures.Ints2TreeNode([]int{1, structures.NULL, 2, 3})},
+			want: []int{1, 3, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := inorderTraversal(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("inorderTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
