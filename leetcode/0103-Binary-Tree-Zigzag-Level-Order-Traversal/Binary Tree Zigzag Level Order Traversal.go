c5d48038729bbe4045e9ecbbd3fbbbc4bd14e0c4
GarryChen <chenymj23@gmail.com>
2022-09-28 11:32:56 +0800

    tree

--- leetcode/0103-Binary-Tree-Zigzag-Level-Order-Traversal/Binary Tree Zigzag Level Order Traversal.go
+++ leetcode/0103-Binary-Tree-Zigzag-Level-Order-Traversal/Binary Tree Zigzag Level Order Traversal.go
@@ -5,11 +5,10 @@ import "algorithm/structures"
 // TreeNode define
 type TreeNode = structures.TreeNode
 
-
 func zigzagLevelOrder(root *TreeNode) [][]int {
 	var res [][]int
 	dfs(root, 0, &res)
-	return res;
+	return res
 }
 
 func dfs(root *TreeNode, level int, res *[][]int) {
@@ -22,13 +21,12 @@ func dfs(root *TreeNode, level int, res *[][]int) {
 		*res = append(*res, []int{})
 	}
 
-	if level % 2 == 0 {
+	if level%2 == 0 {
 		(*res)[level] = append((*res)[level], root.Val)
-	}else
-	{
+	} else {
 		(*res)[level] = append([]int{root.Val}, (*res)[level]...)
 	}
 
-	dfs(root.Left, level + 1, res)
-	dfs(root.Right, level + 1, res)
+	dfs(root.Left, level+1, res)
+	dfs(root.Right, level+1, res)
 }
---
+++ leetcode/0103-Binary-Tree-Zigzag-Level-Order-Traversal/Binary Tree Zigzag Level Order Traversal_test.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+	"reflect"
+	"testing"
+)
+
+func Test_zigzagLevelOrder(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "test",
+			args: args{root: structures.Ints2TreeNode([]int{3, 9, 20, structures.NULL, structures.NULL, 15, 7})},
+			want: [][]int{{3}, {20, 9}, {15, 7}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := zigzagLevelOrder(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("zigzagLevelOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
