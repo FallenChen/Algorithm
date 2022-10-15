fb02a7d439333039b02c42d805642a0925482063
GarryChen <chenymj23@gmail.com>
2022-10-15 16:47:40 +0800

    tree

---
+++ leetcode/0129-Sum-Root-to-Leaf-Numbers/Sum Root to Leaf Numbers_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+	"testing"
+)
+
+func Test_sumNumbers(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test",
+			args: args{root: structures.Ints2TreeNode([]int{4, 9, 0, 5, 1})},
+			want: 1026,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sumNumbers(tt.args.root); got != tt.want {
+				t.Errorf("sumNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
