67c5945341b4c73d925d78b3879a62fd9eaf85dc
GarryChen <chenymj23@gmail.com>
2022-10-19 15:13:48 +0800

    lru

--- leetcode/0146-lru_cache/LRU Cache_test.go
+++ leetcode/0146-lru_cache/LRU Cache_test.go
@@ -2,38 +2,39 @@ package leetcode
 
 import (
 	"fmt"
-	"os"
 	"testing"
 )
 
-var lruCache LRUCache
-
-func setup() {
-	fmt.Println("Before all tests")
-	lruCache = Constructor(2)
-	lruCache.Put(1, 1)
-	lruCache.Put(2, 2)
+func Test_Problem146(t *testing.T) {
+	obj := Constructor(2)
+	fmt.Printf("obj = %v\n", MList2Ints(&obj))
+	obj.Put(1, 1)
+	fmt.Printf("obj = %v\n", MList2Ints(&obj))
+	obj.Put(2, 2)
+	fmt.Printf("obj = %v\n", MList2Ints(&obj))
+	param1 := obj.Get(1)
+	fmt.Printf("param_1 = %v obj = %v\n", param1, MList2Ints(&obj))
+	obj.Put(3, 3)
+	fmt.Printf("obj = %v\n", MList2Ints(&obj))
+	param1 = obj.Get(2)
+	fmt.Printf("param_1 = %v obj = %v\n", param1, MList2Ints(&obj))
+	obj.Put(4, 4)
+	fmt.Printf("obj = %v\n", MList2Ints(&obj))
+	param1 = obj.Get(1)
+	fmt.Printf("param_1 = %v obj = %v\n", param1, MList2Ints(&obj))
+	param1 = obj.Get(3)
+	fmt.Printf("param_1 = %v obj = %v\n", param1, MList2Ints(&obj))
+	param1 = obj.Get(4)
+	fmt.Printf("param_1 = %v obj = %v\n", param1, MList2Ints(&obj))
 }
 
-func teardown() {
-	fmt.Println("After all tests")
-}
-
-func Test1(t *testing.T) {
-	fmt.Println("I'm test1")
-	get := lruCache.Get(1)
-	fmt.Println(get)
-	lruCache.Put(3, 3)
-	fmt.Println(lruCache.Get(2))
-	lruCache.Put(4, 4)
-	fmt.Println(lruCache.Get(1))
-	fmt.Println(lruCache.Get(3))
-	fmt.Println(lruCache.Get(4))
-}
-
-func TestMain(m *testing.M) {
-	setup()
-	code := m.Run()
-	teardown()
-	os.Exit(code)
+func MList2Ints(lru *LRUCache) [][]int {
+	res := [][]int{}
+	head := lru.head
+	for head != nil {
+		tmp := []int{head.Key, head.Value}
+		res = append(res, tmp)
+		head = head.Next
+	}
+	return res
 }
