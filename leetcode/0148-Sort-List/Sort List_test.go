c2dbe114c2ee2d24d9c6fa998d1df588a40d42e8
GarryChen <chenymj23@gmail.com>
2022-10-20 16:13:19 +0800

    sort list

---
+++ leetcode/0148-Sort-List/Sort List_test.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+	"reflect"
+	"testing"
+)
+
+func Test_sortList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "test",
+			args: args{head: structures.Ints2List([]int{4, 2, 1, 3})},
+			want: structures.Ints2List([]int{1, 2, 3, 4}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
