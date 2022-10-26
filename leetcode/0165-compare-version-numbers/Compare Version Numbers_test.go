9a01e997ae6bb98bd7589313030a57774feb07e3
GarryChen <chenymj23@gmail.com>
2022-10-26 16:57:11 +0800

    array

---
+++ leetcode/0165-compare-version-numbers/Compare Version Numbers_test.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import "testing"
+
+func Test_compareVersion(t *testing.T) {
+	type args struct {
+		version1 string
+		version2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test",
+			args: args{version1: "1.0", version2: "1.0.0"},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := compareVersion(tt.args.version1, tt.args.version2); got != tt.want {
+				t.Errorf("compareVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
