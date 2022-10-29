a18739a96b9c0c153ff643c0551a579b465fb698
GarryChen <chenymj23@gmail.com>
2022-10-29 21:25:18 +0800

    array sort

--- leetcode/0179-Largest-Number/Largest Number.go
+++ leetcode/0179-Largest-Number/Largest Number.go
@@ -9,21 +9,20 @@ func largestNumber(nums []int) string {
 	sort.Slice(nums, func(i, j int) bool {
 		x, y := nums[i], nums[j]
 		sx, sy := 10, 10
-		for sx <=x {
+		for sx <= x {
 			sx *= 10
 		}
-		for sy <=y {
+		for sy <= y {
 			sy *= 10
 		}
-		return sy * x + y > sx * y + x
+		return sy*x+y > sx*y+x
 	})
 	if nums[0] == 0 {
 		return "0"
 	}
-	ans := []byte{}
+	var ans []byte
 	for _, x := range nums {
 		ans = append(ans, strconv.Itoa(x)...)
 	}
 	return string(ans)
 }
-
---
+++ leetcode/0179-Largest-Number/Largest Number_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "testing"
+
+func Test_largestNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test",
+			args: args{nums: []int{10, 2}},
+			want: "210",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestNumber(tt.args.nums); got != tt.want {
+				t.Errorf("largestNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
