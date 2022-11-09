35a66e73da60e5fd83f189c638fbb83678254b5f
GarryChen <chenymj23@gmail.com>
2022-11-09 09:12:08 +0800

    stack

--- leetcode/0227-Basic-Calculator-II/Basic Calculator II.go
+++ leetcode/0227-Basic-Calculator-II/Basic Calculator II.go
@@ -1,19 +1,19 @@
 package leetcode
 
-
-
 func calculate(s string) int {
 
-	stack,res,num,preSign := []int{},0,0,'+';
+	var stack []int
+	res, num, preSign := 0, 0, '+'
 
-	for i,ch := range s {
+	for i, ch := range s {
 		isDigit := '0' <= ch && ch <= '9'
 
+		// 考虑位数的情况，比如12，ch为1，2。所以需要进位法
 		if isDigit {
-			num = num *10 + int(ch - '0')
+			num = num*10 + int(ch-'0')
 		}
-		
-		if !isDigit && ch != ' ' || i==len(s)-1 {
+
+		if !isDigit && ch != ' ' || i == len(s)-1 {
 			switch preSign {
 			case '+':
 				stack = append(stack, num)
@@ -27,11 +27,11 @@ func calculate(s string) int {
 			num = 0
 			preSign = ch
 		}
-		 
+
 	}
-	for _,v := range stack {
+	for _, v := range stack {
 		res += v
 	}
 
-	return res;
+	return res
 }
---
+++ leetcode/0227-Basic-Calculator-II/Basic Calculator II_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import "testing"
+
+func Test_calculate(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test",
+			args: args{s: "3+2*2"},
+			want: 7,
+		},
+		{
+			name: "test2",
+			args: args{s: "12+2"},
+			want: 14,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculate(tt.args.s); got != tt.want {
+				t.Errorf("calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
