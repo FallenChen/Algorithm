09148fd59dd2228739647607eecb74e237f14ae2
GarryChen <chenymj23@gmail.com>
2022-11-10 09:08:07 +0800

    tree

---
+++ leetcode/0230-Kth-Smallest-Element-in-a-BST/Kth Smallest Element in a BST_test.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+	"testing"
+)
+
+func Test_kthSmallest(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test",
+			args: args{root: structures.Ints2TreeNode([]int{3, 1, 4, structures.NULL, 2}), k: 1},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := kthSmallest(tt.args.root, tt.args.k); got != tt.want {
+				t.Errorf("kthSmallest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
