3b01ae504d4401407bd0c961e412efc95df3368c
GarryChen <chenymj23@gmail.com>
2022-11-26 14:25:56 +0800

    tree

---
+++ leetcode/0543-Diameter-of-Binary-Tree/Diameter of Binary Tree_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import (
+	"algorithm/structures"
+	"testing"
+)
+
+func Test_diameterOfBinaryTree(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test",
+			args: args{root: structures.Ints2TreeNode([]int{1, 2, 3, 4, 5})},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := diameterOfBinaryTree(tt.args.root); got != tt.want {
+				t.Errorf("diameterOfBinaryTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
