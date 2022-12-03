2013a6e66cf1967a7ab90f44fe088edd1b12612b
GarryChen <chenymj23@gmail.com>
2022-12-03 13:42:14 +0800

    array

--- leetcode/0674-Longest-Continuous-Increasing-Subsequence/Longest Continuous Increasing Subsequence.go
+++ leetcode/0674-Longest-Continuous-Increasing-Subsequence/Longest Continuous Increasing Subsequence.go
@@ -1,25 +1,25 @@
-package	leetcode
+package leetcode
 
 func findLengthOfLCIS(nums []int) int {
 
 	if len(nums) == 0 {
-		return 0;
+		return 0
 	}
 
-	res , length := 1,1
+	res, length := 1, 1
 
-	for i:=1; i<len(nums); i++ {
+	for i := 1; i < len(nums); i++ {
 		if nums[i] > nums[i-1] {
 			length++
 		} else {
-			res = max(res,length)
+			res = max(res, length)
 			length = 1
 		}
 	}
-	return max(res,length)
+	return max(res, length)
 }
 
-func max(a,b int) int {
+func max(a, b int) int {
 
 	if a > b {
 		return a
