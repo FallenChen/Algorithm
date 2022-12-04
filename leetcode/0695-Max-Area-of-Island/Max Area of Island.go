e7d14ef1be59497d8d292ca9c85a486ec635c226
GarryChen <chenymj23@gmail.com>
2022-12-04 10:14:47 +0800

    array

--- leetcode/0695-Max-Area-of-Island/Max Area of Island.go
+++ leetcode/0695-Max-Area-of-Island/Max Area of Island.go
@@ -1,18 +1,16 @@
 package leetcode
 
-
-
 func maxAreaOfIsland(grid [][]int) int {
 
-	res := 0 
+	res := 0
 
 	for i, row := range grid {
 
-		for j,col := range row {
+		for j, col := range row {
 			if col == 0 {
 				continue
 			}
-			area := areaOfIsland(grid, i, j )
+			area := areaOfIsland(grid, i, j)
 			if area > res {
 				res = area
 			}
@@ -21,15 +19,15 @@ func maxAreaOfIsland(grid [][]int) int {
 	return res
 }
 
-func isInGrid(grid [][]int, i,j int) bool {
-	return i >= 0 && i < len(grid) && j >= 0 && j < len(grid[0]) && grid[i][j]==1
+func isInGrid(grid [][]int, i, j int) bool {
+	return i >= 0 && i < len(grid) && j >= 0 && j < len(grid[0]) && grid[i][j] == 1
 }
 
 func areaOfIsland(grid [][]int, x, y int) int {
-	if isInGrid(grid,x,y) {
-		grid[x][y] = 0;
-		return 1 + areaOfIsland(grid, 1+x,y) + areaOfIsland(grid, x-1,y) + 
-				areaOfIsland(grid, x,y-1) + areaOfIsland(grid, x, y+1)
+	if isInGrid(grid, x, y) {
+		grid[x][y] = 0
+		return 1 + areaOfIsland(grid, 1+x, y) + areaOfIsland(grid, x-1, y) +
+			areaOfIsland(grid, x, y-1) + areaOfIsland(grid, x, y+1)
 	}
 	return 0
 }
