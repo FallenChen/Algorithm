69cb9d579c5f8bc932c7c702836fa49ed5334428
GarryChen <chenymj23@gmail.com>
2022-12-09 09:31:23 +0800

    dp

--- leetcode/0887-Super-Egg-Drop/Super Egg Drop.go
+++ leetcode/0887-Super-Egg-Drop/Super Egg Drop.go
@@ -1,14 +1,14 @@
 package leetcode
 
-
+// todo hard
 func superEggDrop(k int, n int) int {
 
-    dp, step := make([]int, k+1), 0 
+	dp, step := make([]int, k+1), 0
 
-    for ; dp[k] < n; step++ {
-        for i:=k; i>0; i-- {
-            dp[i] = 1 + dp[i] + dp[i-1]
-        }
-    }
-    return step
+	for ; dp[k] < n; step++ {
+		for i := k; i > 0; i-- {
+			dp[i] = 1 + dp[i] + dp[i-1]
+		}
+	}
+	return step
 }
